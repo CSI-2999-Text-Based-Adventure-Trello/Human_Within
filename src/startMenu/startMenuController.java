963e88306ed9d343dffed4cae52fa1dd59033066
Mitchell Stankovski <mstankovski@oakland.edu>
2020-10-01 18:04:37 -0400

    Fix up saveMenuController & saveMenuController
    Sends saveGameFile from saveMenuController to saveMenuController. Removed public String saveGame = null; from saveMenuController. Added public void saveGameFile(String savedGame) to startMenuController.

--- src/startMenu/startMenuController.java
+++ src/startMenu/startMenuController.java
@@ -8,26 +8,28 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.stage.Stage;
-import saveMenu.saveMenuController;
 import main.switchBetweenScenes;
 
 public class startMenuController implements Initializable {
 
+    protected String savedGame;
+
     switchBetweenScenes sBS = new switchBetweenScenes();
-    saveMenuController sMC = new saveMenuController();
 
     @FXML
     private void startButtonAction(ActionEvent event) throws IOException {
-        String savedGame = null;
         sBS.newScence("/saveMenu/saveMenu.fxml");
-        String saveGame = sMC.saveGame;
-        System.out.println(sMC.saveGame);
     }
 
     @FXML
     private void tempButtonAction(ActionEvent event) throws IOException {
         Stage thisStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         sBS.switchScence("/gameGUI/gameGUI.fxml", thisStage);
+    }
+    
+    public void saveGameFile(String savedGame) {
+        this.savedGame = savedGame;
+        System.out.println(this.savedGame);
     }
 
     @Override
